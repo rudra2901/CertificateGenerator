c8ce8eba9e2ddde19a44d593f498a84eb3c57ded
Rudra Tiwari <rudratiwari2901@gmail.com>
2022-01-17 19:11:16 +0530

    Changes according to need

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import pandas
 from PIL import Image, ImageFont, ImageDraw
 
 # Global Variables
-FONT_FILE = ImageFont.truetype(r'font/GreatVibes-Regular.ttf', 180)
-FONT_COLOR = "#FFFFFF"
+FONT_FILE = ImageFont.truetype(r'font/Montserrat-Bold.ttf', 31)
+FONT_COLOR = "#000000"
 
 template = Image.open(r'template.png')
 WIDTH, HEIGHT = template.size
+
 
 def make_certificates(name):
     '''Function to save certificates as a .png file'''
@@ -13,21 +15,27 @@ def make_certificates(name):
     image_source = Image.open(r'template.png')
     draw = ImageDraw.Draw(image_source)
 
-    # Finding the width and height of the text. 
+    # Finding the width and height of the text.
     name_width, name_height = draw.textsize(name, font=FONT_FILE)
 
     # Placing it in the center, then making some adjustments.
-    draw.text(((WIDTH - name_width) / 2, (HEIGHT - name_height) / 2 - 30), name, fill=FONT_COLOR, font=FONT_FILE)
+    draw.text(((WIDTH - name_width) / 2, (HEIGHT - name_height) / 2 - 10), name, fill=FONT_COLOR, font=FONT_FILE)
 
     # Saving the certificates in a different directory.
-    image_source.save("./out/" + name +".png")
-    print('Saving Certificate of:', name)        
+    image_source.save("./out/" + name + ".png")
+    print('Saving Certificate of:', name)
+
 
 if __name__ == "__main__":
 
-    names = ['Tushar Nankani', "Full Name", 'Some Long Ass Name Might Not Work']
+    names = []
+
+    with open('names.txt') as f:
+        content = f.readlines()
+        for item in content:
+            names.append(item[:-1].title())
+
     for name in names:
         make_certificates(name)
 
     print(len(names), "certificates done.")
-
